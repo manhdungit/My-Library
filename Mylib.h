5f9fe8782a3af9f2e294018a065cba79094cc96c
Đào Mạnh Dũng <32547045+manhdungit@users.noreply.github.com>
2017-10-13 18:42:29 +0700

    Update Mylib.h

--- Mylib.h
+++ Mylib.h
@@ -82,3 +82,19 @@ int number_of_numbers(long int n)
 	}
 	return count;
 }
+
+/*So uoc so*/
+int number_of_divisor(int n)
+{
+	if (n == 1) return 1;
+	else
+	{
+		if (n == 2) return 2;
+	}
+	int counting = 2;
+	for (int i = 2; i < n; i++)
+	{
+		if (n%i == 0) counting++;
+	}
+	return counting;
+}
